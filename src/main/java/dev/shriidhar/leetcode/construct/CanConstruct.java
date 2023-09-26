4dc6135ce54593fa3a273b33b41df20a39a56fbd
Shriidhar <shridhar.deshmukh3@gmail.com>
2023-09-26 22:54:36 +0200

    Code: memoization approach for canConstruct

--- src/main/java/dev/shriidhar/leetcode/construct/CanConstruct.java
+++ src/main/java/dev/shriidhar/leetcode/construct/CanConstruct.java
@@ -1,14 +1,16 @@
 package dev.shriidhar.leetcode.construct;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class CanConstruct {
 
     /**
      * Brute Force approach to derive if target string can be generated from given dictionary of words
      *
-     * Time Complexity = O(n ^ m) [m = target sum, n = array length]
-     * Space Complexity = O(m)
+     * Time Complexity = O(m * n ^ m) [m = target sum, n = array length]
+     * Space Complexity = O(m ^ 2)
      *
      * @param words  dictionary of words
      * @param target    A target string to be derived
@@ -26,6 +28,45 @@ public class CanConstruct {
             } else if (target.endsWith(word)) {
                 String remainder = target.substring(0, target.indexOf(word));
                 if (bruteForce(words, remainder)) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+
+    /**
+     * Memoization approach to derive if target string can be generated from given dictionary of words
+     *
+     * Time Complexity = O(n * m ^ 2) [m = target sum, n = array length]
+     * Space Complexity = O(m ^ 2)
+     *
+     * @param words  dictionary of words
+     * @param target    A target string to be derived
+     * @return true if target string can be generated from words dictionary, otherwise false.
+     */
+    public static boolean memoization(List<String> words, String target) {
+        Map<String, Boolean> memo = new HashMap<>();
+        return recurse(memo, words, target);
+    }
+
+    private static boolean recurse(Map<String, Boolean> memo, List<String> words, String target) {
+        if (target.isEmpty()) return true;
+        if (memo.containsKey(target)) return memo.get(target);
+
+        for (String word : words) {
+            if (target.startsWith(word)) {
+                String remainder = target.substring(word.length());
+                memo.put(target, recurse(memo, words, remainder));
+                if (memo.get(target)) {
+                    return true;
+                }
+            } else if (target.endsWith(word)) {
+                String remainder = target.substring(0, target.indexOf(word));
+                memo.put(target, recurse(memo, words, remainder));
+                if (memo.get(target)) {
                     return true;
                 }
             }
--- src/test/java/dev/shriidhar/leetcode/construct/CanConstructTest.java
+++ src/test/java/dev/shriidhar/leetcode/construct/CanConstructTest.java
@@ -16,12 +16,18 @@ class CanConstructTest {
         Assertions.assertEquals(expected, CanConstruct.bruteForce(words, target));
     }
 
+    @ParameterizedTest
+    @MethodSource("dataSource")
+    void memoization(List<String> words, String target, boolean expected) {
+        Assertions.assertEquals(expected, CanConstruct.memoization(words, target));
+    }
+
     public static Stream<Arguments> dataSource() {
         return Stream.of(
                 Arguments.of(List.of("ab", "abc", "cd", "def", "abcd"), "abcdef", true),
                 Arguments.of(List.of("bo", "rd", "ska", "ate", "t", "sk", "boar"), "skateboard", false),
                 Arguments.of(List.of("a", "p", "ent", "enter", "ot", "o", "t"), "enterpotentpot", true),
-                Arguments.of(List.of("e", "ee", "eeee", "eeee", "eeeee"), "eeeeeeeeeeeeeeeeeeeeeeeef", false)
+                Arguments.of(List.of("e", "ee", "eeee", "eeee", "eeeee", "eeeeeee", "eeeeee", "eeeeeee"), "eeeeeeeeeeeeeeeeeeeeeeeeeeeef", false)
         );
     }
 }
